d85260a62ccff997ca0521d5d997901add3a5e9f
Sergey87 <serge.techno@gmail.com>
2022-06-07 00:10:11 +0300

    added header, footer, main - only the base part with materializecss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import './App.css';
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Shop from './components/Shop';
 
-class App extends React.Component {}
+function App() {
+  return (
+    <>
+      <Header />
+      <Shop />
+      <Footer />
+    </>
+  );
+}
 
 export default App;
