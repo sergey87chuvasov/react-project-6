a55d857f577adba73b438acf4afe5ab03fe6db76
Sergey87 <serge.techno@gmail.com>
2022-06-07 22:42:47 +0300

    added output of product lists

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,5 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import { API_KEY, API_URL } from '../config';
+import Preloader from './Preloader';
+import GoodList from './GoodsList';
+
 function Shop() {
-  return <main className='container content'>Shop</main>;
+  const [goods, setGoods] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(function getGoods() {
+    fetch(API_URL, {
+      headers: {
+        Authorization: API_KEY,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        data.featured && setGoods(data.featured);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <main className='container content'>
+      {loading ? <Preloader /> : <GoodList goods={goods} />}
+    </main>
+  );
 }
 
 export default Shop;
